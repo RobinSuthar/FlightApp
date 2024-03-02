18aefd70f4d7760772907161abecd3095616657d
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2024-03-02 12:33:23 -0700

    Sui i

--- Views/FlightFinderPage.xaml.cs
+++ Views/FlightFinderPage.xaml.cs
@@ -2,13 +2,34 @@ namespace FlightApp.Views;
 
 public partial class FlightFinderPage : ContentPage
 {
-	public FlightFinderPage()
-	{
-		InitializeComponent();
-	}
+
+
+
+    public FlightFinderPage()
+    {
+        InitializeComponent();
+
+        List<Flight> flights = new List<Flight>()
+    {
+        new Flight { FlightName = "Birtish Airline", Departure = "GLC", Day = "Monday" },
+        new Flight { FlightName = "Qatar  Airline", Departure = "GLC", Day = "Monday" },
+        new Flight { FlightName = "Indian Airline", Departure = "GLC", Day = "Monday" }
+    };
+
+        ListOfFlights.ItemsSource = flights;
+
+    }
 
     private void Reutrn_home_From_finder_page_Clicked(object sender, EventArgs e)
     {
-		Shell.Current.GoToAsync("..");
+        Shell.Current.GoToAsync("..");
+    }
+
+
+    public class Flight()
+    {
+        public string FlightName { get; set; }
+        public string Departure { get; set; }
+        public string Day { get; set; }
     }
 }
